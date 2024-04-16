d8aa2d2f57e66b628eed7dbc99340e5ef58fcc71
ObadaTah <obadatahboub20@gmail.com>
2024-04-17 01:36:25 +0200

    added two pages from MUI kit sginup and sginin also added the MUI kit library to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { getData } from "./ApiFetcher";
-import "./App.css";
+import Footer from "./Components/Generic/Footer";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 
 function App() {
     const [apiData, setApiData] = useState();
 
-    useEffect(() => {
-        getData("academics")
-            .then((data) => {
-                setApiData(data);
-                console.log(apiData);
-            })
-            .catch((error) => {
-                console.error("Error while fetching data:", error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     // getData("academics")
+    //     //     .then((data) => {
+    //     //         setApiData(data);
+    //     //         console.log(apiData);
+    //     //     })
+    //     //     .catch((error) => {
+    //     //         console.error("Error while fetching data:", error);
+    //     //     });
+    // }, []);
     return (
         <>
-            <div></div>
+            <SignUp />
         </>
     );
 }
