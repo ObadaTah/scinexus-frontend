e8b064315658d1908c295a45af9525718de579db
ObadaTah <obadatahboub20@gmail.com>
2024-05-20 01:43:01 +0300

    edits

--- src/Components/Jouranl/Opinion/OpinionsModal.jsx
+++ src/Components/Jouranl/Opinion/OpinionsModal.jsx
@@ -11,7 +11,6 @@ export default function OpinionsModal(props) {
         <Transition in={props.open} timeout={400}>
             {(state) => (
                 <Modal
-                    keepMounted
                     open={!["exited", "exiting"].includes(state)}
                     onClose={() => props.setOpen(false)}
                     slotProps={{
