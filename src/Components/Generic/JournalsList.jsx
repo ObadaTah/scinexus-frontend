f9f054fddbfe8a6814856a95fac30f1ede147396
ObadaTah <obadatahboub20@gmail.com>
2024-05-26 17:11:33 +0300

    asdasf

--- src/Components/Generic/JournalsList.jsx
+++ src/Components/Generic/JournalsList.jsx
@@ -1,131 +1,30 @@
-import { List, ListItem } from "@mui/material";
-import Container from "react-bootstrap/Container";
+import { List, ListItem, Container } from "@mui/material";
+// import Container from "react-bootstrap/Container";
 
 import NewResearchPaper from "../Jouranl/ResearchPaper/NewResearchPaper";
+import SkeletonLoader from "../Jouranl/Post/SkeletonLoader";
 import { useEffect, useState } from "react";
 import { helix } from "ldrs";
 import NewPost from "../Jouranl/Post/NewPost";
+import { useAuth } from "../../Components/contexts/AuthContext";
+import { AspectRatio, Card, Skeleton, Typography } from "@mui/joy";
 
 helix.register();
 
-const dummyOrgs = [
-    { name: "Bethlehem University", image: "https://placehold.co/400x400" },
-    { name: "Bethlehem University", image: "https://placehold.co/400x400" },
-    { name: "Bethlehem University", image: "https://placehold.co/400x400" },
-    { name: "Bethlehem University", image: "https://placehold.co/400x400" },
-];
-const dummyCont = [
-    { name: "Obada Tahboub", image: "https://placehold.co/400x400" },
-    { name: "Obada Tahboub", image: "https://placehold.co/400x400" },
-    { name: "Obada Tahboub", image: "https://placehold.co/400x400" },
-    { name: "Obada Tahboub", image: "https://placehold.co/400x400" },
-];
-const dummyResearchPapers = [
-    {
-        publisher: "UserName",
-        title: "This is a dummy research paper",
-        publishDate: "2021-10-10",
-        organizations: dummyOrgs,
-        image: "https://placehold.co/400x400",
-        contributors: dummyCont,
-        content: {
-            header: "Abstraction",
-            paragraph: "This is a dummy research paper",
-        },
-    },
-    {
-        publisher: "UserName",
-        title: "This is a dummy research paper",
-        publishDate: "2021-10-10",
-        organizations: dummyOrgs,
-        image: "https://placehold.co/400x400",
-        contributors: dummyCont,
-        content: {
-            header: "Abstraction",
-            paragraph: "This is a dummy research paper",
-        },
-    },
-    {
-        publisher: "UserName",
-        title: "This is a dummy research paper",
-        publishDate: "2021-10-10",
-        organizations: dummyOrgs,
-        image: "https://placehold.co/400x400",
-        contributors: dummyCont,
-        content: {
-            header: "Abstraction",
-            paragraph: "This is a dummy research paper",
-        },
-    },
-    {
-        publisher: "UserName",
-        title: "This is a dummy research paper",
-        publishDate: "2021-10-10",
-        organizations: dummyOrgs,
-        image: "https://placehold.co/400x400",
-        contributors: dummyCont,
-        content: {
-            header: "Abstraction",
-            paragraph: "This is a dummy research paper",
-        },
-    },
-];
-const dummyPosts = [
-    {
-        publisher: "UserName",
-        content: "This is a dummy post",
-        publishDate: "2021-10-10",
-    },
-    {
-        publisher: "UserName",
-        content: "This is a dummy post",
-        publishDate: "2021-10-10",
-    },
-    {
-        publisher: "UserName",
-        content: "This is a dummy post",
-        publishDate: "2021-10-10",
-        image: "https://placehold.co/400x400",
-    },
-    {
-        publisher: "UserName",
-        content: "This is a dummy post",
-        publishDate: "2021-10-10",
-        image: "https://placehold.co/3000x3000",
-    },
-];
 function JournalsList() {
     const [posts, setPosts] = useState([]);
     const [researchpapers, setResearchpapers] = useState([]);
     const [isLoading, setIsLoading] = useState("block");
-    async function authenticate() {
-        const response = await fetch(
-            "http://localhost:8080/api/v1/auth/authenticate",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: "obada@gmail.com",
-                    password: "Mohammed1234!",
-                }),
-            }
-        );
-
-        const data = await response.json();
-        return data["jwtToken"];
-    }
+    const { jwtToken } = useAuth();
 
     useEffect(function () {
         async function getAllPosts() {
-            const jwt = await authenticate();
             // console.log(" this is jwt token ", jwt);
             const response = await fetch("http://localhost:8080/posts", {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
-                    Authorization: `Bearer ${jwt}`,
+                    Authorization: `Bearer ${jwtToken}`,
                 },
             });
             if (response.status === 200 || response.status === 201) {
@@ -133,7 +32,7 @@ function JournalsList() {
                 setPosts(data["_embedded"].postList);
                 console.log(data["_embedded"].postList);
             } else {
-                setPosts(dummyPosts);
+                // alert("error")
             }
             setIsLoading("none");
         }
@@ -143,7 +42,6 @@ function JournalsList() {
 
     useEffect(function () {
         async function getAllResearchPapers() {
-            const jwt = await authenticate();
             // console.log(" this is jwt token ", jwt);
             const response = await fetch(
                 "http://localhost:8080/researchpapers",
@@ -151,7 +49,7 @@ function JournalsList() {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
-                        Authorization: `Bearer ${jwt}`,
+                        Authorization: `Bearer ${jwtToken}`,
                     },
                 }
             );
@@ -160,7 +58,7 @@ function JournalsList() {
                 setResearchpapers(data["_embedded"].researchPaperList);
                 console.log(data["_embedded"].researchPaperList);
             } else {
-                setResearchpapers(dummyResearchPapers);
+                // alert("error")
             }
             setIsLoading("none");
         }
@@ -168,50 +66,25 @@ function JournalsList() {
         getAllResearchPapers();
     }, []);
 
-    const onClick = () => {};
     return (
         <>
             <Container
-                fluid
+                // fluid
                 // fixed
                 // maxWidth="false"
                 style={{
                     wordWrap: "break-word",
                     marginTop: "20px",
                     justifyContent: "center",
-                    display: "flex",
+                    // display: "flex",
+                    maxWidth: "60%",
                 }}
             >
-                <l-helix
-                    size="45"
-                    speed="2.5"
-                    color="black"
-                    style={{ display: isLoading }}
+                <SkeletonLoader
+                    style={{ width: "100%" }}
+                    isLoading={isLoading}
                 />
                 <List>
-                    {researchpapers.map((researchPaper, index) => {
-                        return (
-                            <ListItem
-                                key={index}
-                                style={{
-                                    justifyContent: "center",
-                                }}
-                            >
-                                <NewResearchPaper
-                                    opinionsCount={researchPaper.opinionsCount}
-                                    journalId={researchPaper.id}
-                                    publisher={researchPaper.publisher}
-                                    title={researchPaper.title}
-                                    publishDate={researchPaper.createDateTime}
-                                    // organizations={researchPaper.validatedBy}
-                                    image="https://placehold.co/3000x3000"
-                                    content={researchPaper.content}
-                                    contributors={researchPaper.contributors}
-                                    validatedBy={researchPaper.validatedBy}
-                                />
-                            </ListItem>
-                        );
-                    })}
                     {posts.map((post, index) => {
                         return (
                             <ListItem
@@ -226,8 +99,32 @@ function JournalsList() {
                                     opinionsCount={post.opinionsCount}
                                     content={post.content}
                                     publishDate={post.createDateTime}
-                                    image={post.medias[0]}
+                                    images={post.medias}
                                     interactionsCount={post.interactionsCount}
+                                />
+                            </ListItem>
+                        );
+                    })}
+                    {researchpapers.map((researchPaper, index) => {
+                        return (
+                            <ListItem
+                                key={index}
+                                style={{
+                                    justifyContent: "center",
+                                    // minWidth: "100%",
+                                }}
+                            >
+                                <NewResearchPaper
+                                    opinionsCount={researchPaper.opinionsCount}
+                                    journalId={researchPaper.id}
+                                    publisher={researchPaper.publisher}
+                                    title={researchPaper.title}
+                                    publishDate={researchPaper.createDateTime}
+                                    // organizations={researchPaper.validatedBy}
+                                    image="https://placehold.co/3000x3000"
+                                    content={researchPaper.content}
+                                    contributors={researchPaper.contributors}
+                                    validatedBy={researchPaper.validatedBy}
                                 />
                             </ListItem>
                         );
