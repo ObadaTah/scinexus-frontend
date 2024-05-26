aedc1c69b35a2de0276fa86949ae92a8e70eb35d
Mohammed Sowaity <switi.2003@gmail.com>
2024-05-26 17:21:36 +0300

    small change

--- src/Components/Generic/EmailConfirmation.jsx
+++ src/Components/Generic/EmailConfirmation.jsx
@@ -9,6 +9,7 @@ import Grid from "@mui/joy/Grid";
 import { Link } from "react-router-dom";
 
 function EmailConfirmation({ email, open, setOpen }) {
+<<<<<<< Updated upstream
     return (
         <div>
             <Modal
@@ -21,6 +22,63 @@ function EmailConfirmation({ email, open, setOpen }) {
                     justifyContent: "center",
                     alignItems: "center",
                 }}
+=======
+  return (
+    <div>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            width: "650px",
+            height: 400,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            rowGap: 2,
+          }}
+        >
+          <ModalClose variant="plain" sx={{ m: 1 }} />
+          <img
+            src="public\inbox.png"
+            alt="email confirmation"
+            width={100}
+            height={100}
+          />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            fontWeight="lg"
+            mb={1}
+            fontSize={32}
+          >
+            Email Confirmation
+          </Typography>
+          <Typography
+            id="modal-desc"
+            textColor="text.tertiary"
+            sx={{ textAlign: "center", maxWidth: 400 }}
+          >
+            We've sent an email to {email} for you to confirm your email
+            address. Just click the link in the email to complete your
+            registration. Check your inbox{" "}
+            <Link
+              to="http://localhost:1080/#/"
+              style={{ color: "#3aa0ed", textDecoration: "none" }}
+              target="_blank"
+              rel="noopener noreferrer"
+>>>>>>> Stashed changes
             >
                 <Sheet
                     variant="outlined"
